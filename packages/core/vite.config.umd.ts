c5028505f0021cae7492d355815fdd745b82d113
暖阳 <15255096386@163.com>
2024-06-21 14:18:38 +0800

    feat(core): add compression plugin to vite config for umd build

--- packages/core/vite.config.umd.ts
+++ packages/core/vite.config.umd.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import { compression } from 'vite-plugin-compression2';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    compression({
+      include: /.(cjs|css)$/i,
+    }),
+  ],
   build: {
     outDir: 'dist/umd',
     lib: {
