464efccc678b6eb036ff1b18f13de5da5a795ecf
暖阳 <15255096386@163.com>
2024-06-21 14:16:16 +0800

    fix(core): update component exports to use @sunlit-ui/components

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -6,6 +6,6 @@ import '@sunlit-ui/theme/index.css';
 library.add(fas);
 const installer = makeInstaller(components);
 
-export * from '../components';
+export * from '@sunlit-ui/components';
 
 export default installer;
