338eb472863a14b611e8cdd8d7d3fe22a3969e44
暖阳 <15255096386@163.com>
2024-06-24 09:17:44 +0800

    feat(play): add collapse component and update button text in App.vue

--- packages/play/src/App.vue
+++ packages/play/src/App.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const activeNames = ref(['a', 'b']);
+</script>
 
 <template>
-  <n-button type="primary" loading>222222</n-button>
+  <n-button type="primary" loading>按钮</n-button>
+  <n-collapse v-model="activeNames">
+    <n-collapse-item name="a" title="Title a">
+      <div>this is content a</div>
+    </n-collapse-item>
+    <n-collapse-item name="b" title="title b">
+      <div>this is content b</div>
+    </n-collapse-item>
+    <n-collapse-item name="c" title="title c  disable" disabled>
+      <div>this is content c</div>
+    </n-collapse-item>
+  </n-collapse>
 </template>
-
-<style scoped></style>
