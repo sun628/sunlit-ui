fd3de74bba3fe9b211f3b77a6071866b2e3c232f
暖阳 <15255096386@163.com>
2024-06-24 09:18:01 +0800

    refactor(core): optimize build config for production and development environments

--- packages/core/vite.config.es.ts
+++ packages/core/vite.config.es.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
-import { readdirSync } from 'fs';
+import { readdir, readdirSync } from 'fs';
 import vue from '@vitejs/plugin-vue';
-import { filter, map } from 'lodash-es';
+import { defer, delay, filter, map } from 'lodash-es';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import shell from 'shelljs';
+import ModifyFiles from './ModifyFiles';
+import terser from '@rollup/plugin-terser';
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+const isTest = process.env.NODE_ENV === 'test';
+
+const TRY_MOVE_STYLES_DELAY = 800 as const;
 
 function getDirectoriesSync(basePath: string) {
   const entries = readdirSync(basePath, { withFileTypes: true });
@@ -12,14 +21,55 @@ function getDirectoriesSync(basePath: string) {
     (entry) => entry.name
   );
 }
+
+function moveStyles() {
+  readdir('./dist/es/theme', (err) => {
+    if (err) return delay(moveStyles, TRY_MOVE_STYLES_DELAY);
+    defer(() => shell.mv('./dist/es/theme', './dist'));
+  });
+}
+
 export default defineConfig({
   plugins: [
     vue(),
-
     // 生成类型文件
     dts({
       tsconfigPath: '../../tsconfig.build.json',
       outDir: 'dist/types',
+    }),
+    ModifyFiles({
+      rmFiles: ['./dist/es', './dist/theme', './dist/types'],
+      afterBuild: moveStyles,
+    }),
+    terser({
+      // compress`: 压缩选项对象
+      compress: {
+        sequences: isProd,
+        arguments: isProd,
+        drop_console: isProd && ['log'],
+        drop_debugger: isProd,
+        passes: isProd ? 4 : 1,
+        global_defs: {
+          '@DEV': JSON.stringify(isDev),
+          '@PROD': JSON.stringify(isProd),
+          '@TEST': JSON.stringify(isTest),
+        },
+      },
+      format: {
+        semicolons: false,
+        shorthand: isProd,
+        braces: !isProd,
+        beautify: !isProd,
+        comments: !isProd,
+      },
+
+      // `mangle`: 混淆选项对象
+      mangle: {
+        toplevel: isProd,
+        eval: isProd,
+        keep_classnames: isDev,
+        keep_fnames: isDev,
+      },
     }),
   ],
   build: {
