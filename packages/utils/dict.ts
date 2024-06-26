a60167c9bbc2946c7c260c3311dae55e9285afdc
暖阳 <15255096386@163.com>
2024-06-26 09:34:51 +0800

    feat(utils): add typeIconMap dictionary and export it in index.ts

---
+++ packages/utils/dict.ts
@@ -0,0 +1,7 @@
+export const typeIconMap = new Map([
+  ['info', 'circle-info'],
+  ['success', 'check-circle'],
+  ['warning', 'circle-exclamation'],
+  ['danger', 'circle-xmark'],
+  ['error', 'circle-xmark'],
+]);
