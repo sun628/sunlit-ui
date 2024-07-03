79c52fb74e8671a1c9a83d753b36b0a32a0ba129
暖阳 <15255096386@163.com>
2024-07-03 13:25:08 +0800

    feat(vitest): configure Vitest and setup test environment

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  define: {
+    PROD: JSON.stringify(false),
+    DEV: JSON.stringify(false),
+    TEST: JSON.stringify(true),
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/true/coverage/**',
+      '**/coverage/**',
+    ],
+    setupFiles: [resolve(__dirname, './vitest.setup.ts')],
+  },
+});
