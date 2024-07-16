bbcc956dcc34f09744f8e185b790c89610b90826
nuanyang <15255096386@163.com>
2024-07-16 21:09:36 +0800

    Delete packages/components/Button directory

--- packages/components/Button/index.ts
+++
@@ -1,6 +0,0 @@
-import Button from './Button.vue';
-import ButtonGroup from './ButtonGroup.vue';
-import { withInstall } from '@sunlit-ui/utils';
-
-export const NButton = withInstall(Button);
-export const NButtonGroup = withInstall(ButtonGroup);
