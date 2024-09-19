c60f9de5ee17c3b6e0964c70ce6a3a4b48083ba5
nuanyang <15255096386@163.com>
2024-09-19 09:59:10 +0800

    Delete packages/components/collapse directory

--- packages/components/collapse/constants.ts
+++
@@ -1,4 +0,0 @@
-import type { InjectionKey } from 'vue';
-import type { CollapseContext } from './types';
-
-export const COLLAPSE_CTX_KEY: InjectionKey<CollapseContext> = Symbol('collapseContext');
--- packages/components/collapse/index.ts
+++
@@ -1,8 +0,0 @@
-import Collapse from './Collapse.vue';
-import CollapseItem from './CollapseItem.vue';
-import { withInstall } from '@sunlit-ui/utils';
-
-export const NCollapse = withInstall(Collapse);
-export const NCollapseItem = withInstall(CollapseItem);
-
-export * from './types';
