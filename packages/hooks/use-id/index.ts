dab9a5db8da05b10a273a3ddac1c53bbfda22485
暖阳 <15255096386@163.com>
2024-09-19 09:57:03 +0800

    feat: add useId hook for unique ID generation

---
+++ packages/hooks/use-id/index.ts
@@ -0,0 +1,16 @@
+import { type Ref, computed } from 'vue'
+
+const defaultIdInjection = {
+  prefix: Math.floor(Math.random() * 10000),
+  current: 0,
+}
+
+export function useId(namespace: string = 'n'): Ref<string> {
+  const idRef = computed(
+    () => `${namespace}-id-${defaultIdInjection.prefix}-${defaultIdInjection.current++}`
+  )
+
+  return idRef
+}
+
+export default useId
