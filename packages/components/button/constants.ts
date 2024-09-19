580e1d0ac0a33a2b053b3d7d2d954ac36fbdd0f5
nuanyang <15255096386@163.com>
2024-09-19 09:59:00 +0800

    Delete packages/components/button directory

--- packages/components/button/constants.ts
+++
@@ -1,5 +0,0 @@
-import type { InjectionKey } from 'vue';
-import type { ButtonGroupContext } from './types';
-
-export const BUTTON_GROUP_CTX_KEY: InjectionKey<ButtonGroupContext> =
-  Symbol('buttonGroupContext');
