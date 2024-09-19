42af8e830dbe28f34b5bfd84e06ec730c0606dbe
暖阳 <15255096386@163.com>
2024-09-19 14:19:35 +0800

    feat: refactor useLocale to simplify code

--- packages/hooks/use-locale/index.ts
+++ packages/hooks/use-locale/index.ts
@@ -1,30 +1,31 @@
-import { inject, ref, unref, computed, type Ref } from 'vue'
+import { inject, type Ref } from 'vue'
 import { omit } from 'lodash-es'
 import { createI18n, i18nSymbol, type I18nInstance } from 'vue3-i18n'
 import type { Language } from '@sunlit-ui/locale'
 import English from '@sunlit-ui/locale/lang/en'
 
-const omitInstall = (obj: I18nInstance) => omit(obj, 'install')
-
 export function useLocale(localeOverrides?: Ref<Language>) {
   if (!localeOverrides) {
-    const i18n: Ref<I18nInstance> =
-      inject(i18nSymbol) ??
-      ref(createI18n({ locale: English.name, messages: { en: English.el } }))
-
-    return computed(() => omitInstall(unref(i18n)))
+    return omit(
+      <I18nInstance>(
+        inject(
+          i18nSymbol,
+          createI18n({ locale: English.name, messages: { en: English.el } })
+        )
+      ),
+      'install'
+    )
   }
 
-  return computed(() =>
-    omitInstall(
-      createI18n({
-        locale: localeOverrides.value.name,
-        messages: {
-          en: English.el,
-          [localeOverrides.value.name]: localeOverrides.value.el,
-        },
-      })
-    )
+  return omit(
+    createI18n({
+      locale: localeOverrides.value.name,
+      messages: {
+        en: English.el,
+        [localeOverrides.value.name]: localeOverrides.value.el,
+      },
+    }),
+    'install'
   )
 }
 
